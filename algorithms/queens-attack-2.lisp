ba491f5d6e32cb011423dea148758a647816bcfe
Nisen <imnisen@gmail.com>
2017-07-01 18:14:14 +0800

    暂存，算法未完成

--- algorithms/queens-attack-2.lisp
+++ algorithms/queens-attack-2.lisp
@@ -16,29 +16,140 @@
   (let ((line (read-line)))
     (split-string-to-integer-list line)))
 
-(defun count-direction (rq-cq ri-ci-list direction)
-  (cond ((= 1 direction) (count-direction-1 rq-cq ri-ci-list))
-        ((= 2 direction) (count-direction-2 rq-cq ri-ci-list))
-        ((= 3 direction) (count-direction-3 rq-cq ri-ci-list))
-        ((= 4 direction) (count-direction-4 rq-cq ri-ci-list))
-        ((= 5 direction) (count-direction-5 rq-cq ri-ci-list))
-        ((= 6 direction) (count-direction-6 rq-cq ri-ci-list))
-        ((= 7 direction) (count-direction-7 rq-cq ri-ci-list))
-        ((= 8 direction) (count-direction-8 rq-cq ri-ci-list))
-        (t nil)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 业务函数
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun count-direction-1 (rq-cq ri-ci-list)
-  "假设ri-ci-list是分好组的"
-  (- (first (first (sort ri-ci-list #'< :key #'second))) (first rq-cq) 1))
+
+(defun count-direction (rq-cq ri-ci-list n)
+  (let* ((group-list (my-group rq-cq ri-ci-list))
+         (first-list (my-sort (first group-list)))
+         (second-list (second group-list))
+         (third-list (my-sort (third group-list)))
+         (fourth-list (fourth group-list))
+         (fifth-list (my-sort (fifth group-list)))
+         (sixth-list (sixth group-list))
+         (seventh-list (my-sort (seventh group-list)))
+         (eighth-list (eighth group-list)))
+    (+ (count-direction-1 rq-cq first-list n)
+       (count-direction-2 rq-cq second-list n)
+       (count-direction-3 rq-cq third-list n)
+       (count-direction-4 rq-cq fourth-list n)
+       (count-direction-5 rq-cq fifth-list n)
+       (count-direction-6 rq-cq sixth-list n)
+       (count-direction-7 rq-cq seventh-list n)
+       (count-direction-8 rq-cq eighth-list n))))
+
+(defun count-direction-1 (rq-cq a-list n)
+  (if (null a-list)
+      (- n (first rq-cq))
+      (- (first (first a-list)) (first rq-cq) 1)))
+
+(defun count-direction-2 (rq-cq a-list n)
+  (let ((count 0)
+        (rq (first rq-cq))
+        (cq (second rq-cq)))
+    (loop
+       for r from (1+ rq) to n
+       for c from (1+ cq) to n
+       do (if (not (d2-in-p r c a-list))
+              (incf count)
+              (return)))
+    count))
+
+(defun count-direction-3 (rq-cq a-list n)
+  (if (null a-list)
+      (- n (second rq-cq))
+      (- (second (first a-list)) (second rq-cq) 1)))
+
+(defun count-direction-4 (rq-cq a-list n)
+  (let ((count 0)
+        (rq (first rq-cq))
+        (cq (second rq-cq)))
+    (loop
+       for r from (1- rq) downto 1
+       for c from (1+ cq) to n
+       do (if (not (d4-in-p r c a-list))
+              (incf count)
+              (return)))
+    count))
+
+(defun count-direction-5 (rq-cq a-list n)
+  (declare (ignore n))
+  (if (null a-list)
+      (- (first rq-cq) 1)
+      (- (first rq-cq) (first (first (last a-list))) 1)))
+
+(defun count-direction-6 (rq-cq a-list n)
+  (declare (ignore n))
+  (let ((count 0)
+        (rq (first rq-cq))
+        (cq (second rq-cq)))
+    (loop
+       for r from (1- rq) downto 1
+       for c from (1- cq) downto 1
+       do (if (not (d6-in-p r c a-list))
+              (incf count)
+              (return)))
+    count))
+
+(defun count-direction-7 (rq-cq a-list n)
+  (declare (ignore n))
+  (if (null a-list)
+      (- (second rq-cq) 1)
+      (- (second rq-cq) (second (first (last a-list))) 1)))
+
+(defun count-direction-8 (rq-cq a-list n)
+  (let ((count 0)
+        (rq (first rq-cq))
+        (cq (second rq-cq)))
+    (loop
+       for r from (1+ rq) to n
+       for c from (1- cq) downto 1
+       do (if (not (d8-in-p r c a-list))
+              (incf count)
+              (return)))
+    count))
+
+(defun d2-in-p (r c a-list)
+  (loop named outer for a in a-list do
+       (if (and (> (first a) r) (> (second a) c))
+           (return-from outer nil)
+           (if (and (= (first a) r) (= (second a) c))
+               (return-from outer T)))
+     finally (return-from outer nil)))
+
+;; 算法需优化
+(defun d4-in-p (r c a-list)
+  (loop named outer for a in a-list do
+       (if (and (= r (first a))
+                (= c (second a)))
+           (return-from outer T))
+     finally (return-from outer nil)))
+
+;; 算法需优化
+(defun d6-in-p (r c a-list)
+  (loop named outer for a in a-list do
+       (if (and (= r (first a))
+                (= c (second a)))
+           (return-from outer T))
+     finally (return-from outer nil)))
+
+;; 算法需优化
+(defun d8-in-p (r c a-list)
+  (loop named outer for a in a-list do
+       (if (and (= r (first a))
+                (= c (second a)))
+           (return-from outer T))
+     finally (return-from outer nil)))
+
+
 
 (defun list< (a b)
   (cond ((null a) (not (null b)))
         ((null b) nil)
         ((= (first a) (first b)) (list< (rest a) (rest b)))
         (t (< (first a) (first b)))))
-;; TODO
-(defun count-direction-2 (rq-cq ri-ci-list)
-  )
 
 (defun my-sort (a-list)
   (sort (copy-seq a-list) #'list<))
@@ -47,14 +158,24 @@
   (let ((rq (first rq-cq))
         (cq (second rq-cq)))
     (loop for (r c) in a-list
-       if (and (= r rq) (> c cq)) collecting (list r c) into d1
-       finally (return d1))))
+       if (and (= r rq) (> c cq)) collecting (list r c) into d3
+       else if (and (= r rq) (< c cq)) collecting (list r c) into d7
+       else if (and (> r rq) (= c cq)) collecting (list r c) into d1
+       else if (and (< r rq) (= c cq)) collecting (list r c) into d5
+       else if (and (< r rq) (< c cq)) collecting (list r c) into d6
+       else if (and (< r rq) (> c cq)) collecting (list r c) into d4
+       else if (and (> r rq) (> c cq)) collecting (list r c) into d2
+       else if (and (> r rq) (< c cq)) collecting (list r c) into d8
+       finally (return (list d1 d2 d3 d4 d5 d6 d7 d8)))))
 
-(defun test-make-list (n)
-  (loop for i from 1 to n do
-       (loop for j from 1 to n
-          collectint (list i j) into r)
-     finally (return r)))
+;; (defun test-make-list (n)
+;;   (loop for i from 1 to n
+;;      append  (loop for j from 1 to n
+;;                 collect (list i j))))
+
+;; (defun p-print (a-list)
+;;   (loop for x in a-list do
+;;        (format t "~a~&" x)))
 
 
 (defun main ()
@@ -65,4 +186,4 @@
          (a-list '())
          (ri-ci-list (dotimes (i k a-list)
                        (setf a-list (append a-list (list (read-line-to-int-list)))))))
-    ))
+    (count-direction rq-cq ri-ci-list n)))
